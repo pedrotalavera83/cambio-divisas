55053bc738318834d3e59e88ef8101d9faaadb71
pedro Pablo Talavera Segura <pedrotalaveras@gmail.com>
2024-11-01 15:29:12 -0600

    feat: uso de la api

---
+++ src/api/RateAPI.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const baseURL = 'https://api.frankfurter.app/';
+
+export const fetchCurrecies = async () => {
+    try {
+        const response = await axios.get(`${baseURL}currencies`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching currencies: ', error);
+    }
+};
+
+export const fetchRates = async (date: string, base: string, symbols: string) => {
+    try {
+        const response = await axios.get(`${baseURL}${date}?base=${base}&symbols=${symbols}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching currencies: ', error);
+    }
+};
